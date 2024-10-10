21a6fef2b63dfb0ed0580d2359b332a8626b5453
Mayur Pratap Singh <mayur.pratap.singh.150902@gmail.com>
2024-10-10 17:39:45 +0530

    i dont know why both apis for post are not working

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -1,14 +1,17 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class JournalEntryService {
 
@@ -20,7 +23,13 @@ public class JournalEntryService {
     }
 
     public void saveEntry(JournalEntry journalEntry){
-        journalEntryRepository.save(journalEntry);
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        } catch (Exception e) {
+            log.error("Exception", e);
+        }
+
     }
     public Optional<JournalEntry> findById(ObjectId id){
         return journalEntryRepository.findById(id);
